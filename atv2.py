152a59538f52567db7ed232c211190811cf4442e
Fernando José da Silva <fernando.dev21@outlook.com>
2024-08-26 11:02:22 -0300

    first commit

---
+++ atv2.py
@@ -0,0 +1,58 @@
+def saudacao():
+    print("Olá mundo!")
+
+# chamando a função
+saudacao()
+
+def saudacao_personalizada(nome):
+    print("Olá,", nome + "!")
+
+saudacao_personalizada("Maria")
+
+def soma(a, b):
+    resultado = a + b
+    return resultado
+total = soma(3, 5)
+print(total)
+
+def somar(x, y):
+    return x + y
+
+def subtrair(x, y):
+    return x - y 
+
+def multiplicar(x, y):
+    return x * y
+
+def dividir(x, y):
+    if y == 0:
+        return "Erro: Divisão por zero!"
+    else:
+        return x / y
+    
+# Obtendo entrada do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Menu de operações
+print("Escolha a operação:")
+print("1. Somar")
+print("2. Subtrair")
+print("3. Multiplicar")
+print("4. Dividir")
+
+escolha = input("Digite sua escolha (1/2/3/4): ")
+
+# Realizando o operação e exibindo o resultado
+if escolha == '1':
+    print(num1, "+", num2, "=", somar(num1, num2))
+elif escolha == '2':
+    print(num1, "-", num2, "=", subtrair(num1, num2))
+elif escolha == '3':
+    print(num1, "*", num2,
+    "=", multiplicar(num1, num2))
+elif escolha == '4':
+    print(num1, "/", num2, "=", dividir(num1, num2))
+
+else:
+    print("Escolhar inválida!")
